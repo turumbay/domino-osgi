497d4b98c6d96dcd37fc8bea1f2410d9226ca0a9
Ilya Golberg <turumbay@gmail.com>
2013-04-07 22:22:22 +0400

    Servlet with Declarative Services

--- money-utils-ws/src/main/java/ru/turumbay/blog/osgi/money/ws/internal/CurrencyRateServlet.java
+++ money-utils-ws/src/main/java/ru/turumbay/blog/osgi/money/ws/internal/CurrencyRateServlet.java
@@ -1,6 +1,11 @@
 package ru.turumbay.blog.osgi.money.ws.internal;
 
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 import ru.turumbay.blog.osgi.money.Currency;
 import ru.turumbay.blog.osgi.money.CurrencyService;
 
@@ -11,9 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
+@Component
 public class CurrencyRateServlet extends HttpServlet {
 
+    @Reference
     CurrencyService rateService;
+
+    @Reference
+    HttpService httpService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,4 +37,11 @@ public class CurrencyRateServlet extends HttpServlet {
         }
     }
 
+    protected void activate(ComponentContext ctx) throws ServletException, NamespaceException {
+       httpService.registerServlet("/rates/USD", this, null, null);
+    }
+
+    protected void deactivate(ComponentContext ctx) {
+       httpService.unregister("/rates/USD");
+    }
 }
